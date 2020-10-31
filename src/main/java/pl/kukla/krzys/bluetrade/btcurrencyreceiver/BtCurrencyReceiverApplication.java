dc404417d9820dce28e28fd51ea6144ffe6dea3b
Krzysztof Kukla <krzys.kukla@gmail.com>
2020-10-31 22:39:53 +0100

    Initial commit

---
+++ src/main/java/pl/kukla/krzys/bluetrade/btcurrencyreceiver/BtCurrencyReceiverApplication.java
@@ -0,0 +1,13 @@
+package pl.kukla.krzys.bluetrade.btcurrencyreceiver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtCurrencyReceiverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BtCurrencyReceiverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/kukla/krzys/bluetrade/btcurrencyreceiver/BtCurrencyReceiverApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kukla.krzys.bluetrade.btcurrencyreceiver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BtCurrencyReceiverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
